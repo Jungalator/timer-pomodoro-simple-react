f3f1e1ba34e8efbc6fc4f29d03039c0a33326a06
Alex_Wondermaker <papajohn.strike@gmail.com>
2025-08-06 16:06:37 +0300

    fixed some styles

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -2,11 +2,11 @@ import { NavButton } from "./NavButton";
 
 export const Nav = ({ toggleVisible, setVisibleSettings, setVisibleStats }) => {
   return (
-    <nav className="ml-auto flex justify-around w-[40%]">
+    <nav className="ml-auto flex justify-end w-[100%]">
       <NavButton
         toggleVisible={() => toggleVisible(setVisibleStats)}
         buttonIcon="☑︎"
-        buttonTitle="Statistic"
+        buttonTitle="Statistics"
       />
       <NavButton
         toggleVisible={() => toggleVisible(setVisibleSettings)}
--- src/components/Nav/NavButton.jsx
+++ src/components/Nav/NavButton.jsx
@@ -3,7 +3,7 @@ export const NavButton = ({ toggleVisible, buttonIcon, buttonTitle }) => {
     <>
       <button
         onClick={toggleVisible}
-        className=" flex items-center text-white hover:text-neutral-300  cursor-pointer font-semibold"
+        className="ml-4 flex items-center text-white hover:text-neutral-300  cursor-pointer font-semibold"
       >
         <span className="text-3xl h-10 mr-1.5">{buttonIcon}</span> {buttonTitle}
       </button>
